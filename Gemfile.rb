3e785143ec6d6b6bd8b19422ca53bb97e8cd6e6c
Judson L Moore <judsonlmoore@gmail.com>
2020-07-29 14:34:08 +0200

    Update workflow

---
+++ Gemfile.rb
@@ -0,0 +1,23 @@
+source "https://rubygems.org"
+
+gem "liquid-c"
+
+# If you have any plugins, put them here!
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+
+gem 'jekyll'
+
+#Plugins here
+group :jekyll_plugins do
+    gem 'jekyll-feed'
+    gem 'jekyll-paginate'
+    gem 'jekyll-seo-tag'
+    gem 'jekyll-archives'
+    gem 'kramdown'
+    gem 'rouge'
+    gem 'jekyll-cloudinary'
+    gem 'jekyll-sitemap'
+    gem 'jekyll-algolia'
+    gem 'html-proofer'
+    gem 'jekyll-target-blank'
+end
